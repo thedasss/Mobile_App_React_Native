697f24bd148f6163760a99b103c70a4af7560551
Thedas Silva <thedassilva64@gmail.com>
2026-01-04 15:50:37 +0530

    commit
    updated navigate button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-gray-800 font-bold">
-        Welcome!
+        Welcome!1
       </Text>
 
     </View>
