4ecb3cae1faee0067ab3f08ab3ec064dcae5c385
Thedas Silva <thedassilva64@gmail.com>
2026-01-07 21:05:59 +0530

    commit

---
+++ components/SearchBar.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export const SearchBar = () => {
+  return (
+    <View className="w-full bg-white rounded-lg p-3 shadow-md">
+      <Text className="text-gray-500">Search Bar Placeholder</Text>
+    </View>
+  );
+};
+
+export default SearchBar;
